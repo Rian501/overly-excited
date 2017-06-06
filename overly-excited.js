05cdb6c2b7b50e44170950abca2e0b4bc4573d5e
Rian501 <eclaire.lemmon@gmail.com>
2017-06-06 13:55:46 -0500

    Add multiple subl . after each iteration based on how many times it is divisible by 3

--- overly-excited.js
+++ overly-excited.js
@@ -1,3 +1,35 @@
+// // Create an array that contains the words in the sentence
+// var sentence = ["The", "walrus", "danced", "through", "the", "trees", "in", "the", "light", "of", "the", "moon"];
+
+// var addOnSent = ""
+
+// // The addExcitement function should accept the array as the sole argument
+// function addExcitement (theWordArray) {
+    
+//       Write a `for` loop that iterates over the array argument and
+//       outputs the words.
+     
+
+
+//      for (i = 0; i < theWordArray.length; i++) {
+//      	addOnSent = addOnSent +' '+ sentence[i];
+
+//      	if ((i+1) % 3 === 0) {
+//      		addOnSent = addOnSent + '! ';
+//      	}
+
+
+//      	console.log(addOnSent);
+
+//      }
+// }
+
+// // Invoke the function and pass in the array
+// addExcitement(sentence);
+
+
+
+
 // Create an array that contains the words in the sentence
 var sentence = ["The", "walrus", "danced", "through", "the", "trees", "in", "the", "light", "of", "the", "moon"];
 
@@ -10,13 +42,20 @@ function addExcitement (theWordArray) {
       outputs the words.
      */
 
+	var exclams = ''
 
      for (i = 0; i < theWordArray.length; i++) {
      	addOnSent = addOnSent +' '+ sentence[i];
 
+
+
      	if ((i+1) % 3 === 0) {
-     		addOnSent = addOnSent + '! ';
+     			     	
+	     	exclams = exclams + '!';
+
+     		addOnSent = addOnSent + exclams;
      	}
+
 
 
      	console.log(addOnSent);
@@ -55,7 +94,9 @@ addExcitement(sentence);
 // ...
 // Challenge
 
-// Add logic in the addExcitement function to the increase the number of exclamation points after every third word. The number of exclamation points (!) will be determined by how many times the counter variable can be divided by 3.
+// Add logic in the addExcitement function to the increase the number of exclamation points 
+//after every third word. The number of exclamation points (!) will be determined by how many times 
+//the counter variable can be divided by 3.
 
 // Example output:
 
